99d1b0f1fd4e3c1050bd1593c855f33596d866b7
dioneybecker <dioneybecker@gmail.com>
2024-06-09 08:51:30 -0300

    using mapping to handle money deposited by contract

---
+++ contracts/ExampleMapping2.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract BalanceMapping {
+    
+    mapping(address => uint) public balances;
+
+    function deposti() public payable {
+        balances[msg.sender] = msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
+        return balances[msg.sender];
+    }
+
+    function withdrawaAllMoney(address payable _to) public payable {
+        _to.transfer(getBalance());
+    }
+}
